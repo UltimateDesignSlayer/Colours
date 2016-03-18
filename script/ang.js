ad4839329ad6762839cbc73221194fd7a558641e
UltimateDesignSlayer <farhadinho99@googlemail.com>
2016-03-18 08:43:10 +0000

    angular stuff added.

---
+++ script/ang.js
@@ -0,0 +1,23 @@
+﻿var angularStart = angularStart || {};
+
+angularStart = {
+    init: function () {
+        this.controller();
+    },
+    dataModel: {
+        title: 'Foobar',
+        text: 'This is some text here.'
+    },
+    controller: function () {
+        var that = this;
+
+        var app = angular.module('textOutputter', []);
+
+        app.controller('textController', function ($scope) {
+            $scope.heading = that.dataModel.title;
+            $scope.text = that.dataModel.text;
+        });
+    }
+}
+
+angularStart.init();
